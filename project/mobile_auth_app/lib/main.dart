4941912453b5199129a4a34942b7f89177bcf445
ionut <ionut.roma9@gmail.com>
2025-11-09 08:55:09 +0100

    aggiunta source code tesi

--- project/mobile_auth_app/lib/main.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'models/session_manager.dart';
-import 'app.dart';
-
-void main() {
-  runApp(ChangeNotifierProvider(create: (_) => SessionManager(), child: MyApp()));
-}
--- project/mobile_auth_app/lib/widgets/logo.dart
+++
@@ -1,5 +0,0 @@
-import 'package:flutter/material.dart';
-
-Widget buildLogo() {
-  return Image.asset('assets/logo.png', height: 280);
-}
